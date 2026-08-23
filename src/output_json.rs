c72d3cbd4257005a46e50544703388f242432ce7
Sonny Falk <sonnyfalk@gmail.com>
2026-08-23 12:50:58 -0700

    Output conflicting files in json format.

--- src/output_json.rs
+++ src/output_json.rs
@@ -10,6 +10,7 @@ struct Output {
     root_workspace: Workspace,
     dependencies: Vec<Workspace>,
     tree: DependencyNode,
+    conflicting_files: Vec<ConflictingFile>,
 }
 
 #[derive(Debug, Serialize)]
@@ -27,20 +28,36 @@ struct DependencyNode {
     missing_dependencies: Vec<String>,
 }
 
+#[derive(Debug, Serialize)]
+struct ConflictingFile {
+    name: String,
+    candidates: Vec<ConflictingFileCandidate>,
+    df26: String,
+    df25: String,
+}
+
+#[derive(Debug, Serialize)]
+struct ConflictingFileCandidate {
+    workspace: String,
+    path: PathBuf,
+}
+
 pub fn analyze_and_output_json(root_workspace: Workspace) {
     let tree = WorkspaceDependencyTree::new(root_workspace);
-    let json_tree = json_workspace_dependency_tree(&tree);
+    let json_tree = workspace_dependency_tree(&tree);
+    let conflicting_files = conflicting_files(&tree);
     let (root_workspace, dependencies) = tree.to_root_workspace_and_dependencies();
     let output = Output {
         version: clap::crate_version!().into(),
         root_workspace: root_workspace,
         dependencies: dependencies,
         tree: json_tree,
+        conflicting_files: conflicting_files,
     };
     println!("{}", serde_json::to_string_pretty(&output).unwrap());
 }
 
-fn json_workspace_dependency_tree(tree: &WorkspaceDependencyTree) -> DependencyNode {
+fn workspace_dependency_tree(tree: &WorkspaceDependencyTree) -> DependencyNode {
     fn node(
         workspace: &Workspace,
         tree: &WorkspaceDependencyTree,
@@ -114,4 +131,39 @@ fn json_workspace_dependency_tree(tree: &WorkspaceDependencyTree) -> DependencyN
     }
 
     node(tree.root_workspace(), tree, &mut HashSet::new())
+}
+
+fn conflicting_files(tree: &WorkspaceDependencyTree) -> Vec<ConflictingFile> {
+    let all_duplicate_filenames = tree.all_duplicate_filenames();
+    all_duplicate_filenames
+        .into_iter()
+        .map(|(file, dependency)| {
+            let candidates = dependency
+                .all()
+                .into_iter()
+                .filter_map(|candidate| {
+                    tree.source_file_in_workspace(&file, candidate).map(|p| {
+                        ConflictingFileCandidate {
+                            workspace: candidate.name().to_string(),
+                            path: p.clone(),
+                        }
+                    })
+                })
+                .collect();
+            let resolved26 = match tree.resolve_workspace_dependency_df26(dependency.clone()) {
+                WorkspaceDependency::Dependency(dep) => Some(dep.name().to_string()),
+                _ => None,
+            };
+            let resolved25 = match tree.resolve_workspace_dependency_df25(dependency.clone()) {
+                WorkspaceDependency::Dependency(dep) => Some(dep.name().to_string()),
+                _ => None,
+            };
+            ConflictingFile {
+                name: file.to_string(),
+                candidates: candidates,
+                df26: resolved26.unwrap_or("(Unresolved)".into()),
+                df25: resolved25.unwrap_or("(Unresolved)".into()),
+            }
+        })
+        .collect()
 }
